389c0abe665b90e86043404f431ff2c8b67723a1
Bartosz Wróbel <maxoen@gmail.com>
2013-12-02 11:30:32 +0100

    First version

---
+++ jscrambler.gemspec
@@ -0,0 +1,11 @@
+Gem::Specification.new do |s|
+  s.name        = 'jscrambler'
+  s.version     = '0.1.0'
+  s.date        = '2013-11-15'
+  s.summary     = "JScrambler Web API ruby client"
+  s.description = "Client of JScrambler http://jscrambler.com"
+  s.authors     = ["Bartosz Wrobel"]
+  s.email       = 'maxoen@gmail.com'
+  s.files       = ["lib/jscrambler.rb"]
+  s.license     = 'MIT'
+end
---
+++ lib/jscrambler.rb
@@ -0,0 +1,103 @@
+require 'base64'
+require 'openssl'
+require 'uri'
+require 'rest-client'
+require 'json'
+
+class Jscrambler
+
+  API_URL = 'http://api.jscrambler.com/v3'
+
+  def initialize(access_key, secret_key)
+    @access_key = access_key
+    @secret_key = secret_key
+
+    @files_content = nil
+    @files_md5 = nil
+  end
+
+  def upload_code(files, params)
+    action = '/code.json'
+
+    prepare_files_data(files)
+
+    query_params = {}
+    #add default params
+    query_params['timestamp']  = Time.now.utc.iso8601.to_s
+    query_params['access_key'] = @access_key
+    query_params['signature'] = generate_signature('POST', action, query_params.merge(@files_md5))
+    
+    request = RestClient::Request.new(
+                                      :method => :post,
+                                      :url => "#{API_URL}#{action}",
+                                      :payload => {
+                                                   :multipart => true                                                     
+                                                  }.merge(query_params).merge(@files_content).merge(params)
+                                     )
+
+    JSON.parse(request.execute)
+  end
+
+  def get_information
+  end
+
+  def download_code
+  end
+
+  def delete_code(project_id)
+    action = "/code/#{project_id}.zip"
+
+    query_params = {}
+    #add default params
+    query_params['timestamp']  = Time.now.utc.iso8601.to_s
+    query_params['access_key'] = @access_key
+    query_params['signature'] = generate_signature('DELETE', action, query_params)
+
+    request = RestClient::Request.new(
+                                      :method => :delete,
+                                      :url => "#{API_URL}#{action}?#{query_string(query_params)}"
+                                     )
+
+    JSON.parse(request.execute)
+  end
+
+  private
+    
+  def generate_signature(request_method, resource_path, params)
+    Base64.strict_encode64(OpenSSL::HMAC.digest(
+                                                OpenSSL::Digest::SHA256.new, 
+                                                @secret_key, 
+                                                "#{request_method};#{api_hostname};#{resource_path};#{query_string(params)}"
+                                               )
+                          )
+  end
+
+  def api_hostname
+    URI(API_URL).hostname
+  end
+
+  def query_string(params)
+    items = []
+    params.keys.sort.each do |key|
+      items.push("#{key}=#{url_encode(params[key])}")
+    end
+
+    items.join('&')
+  end
+
+  def url_encode(string)
+    str = CGI::escape(string)
+    str = str.sub('%7E', '~')
+    str = str.sub('+', '%20')
+  end
+
+  def prepare_files_data(files)
+    @files_md5 = {}
+    @files_content = {}
+    files.each do |file|
+      @files_md5["file_#{@files_md5.size}"] = Digest::MD5.hexdigest(File.read(file))
+      @files_content["file_#{@files_content.size}"] = File.new(file, 'rb')
+    end
+  end
+  
+end
